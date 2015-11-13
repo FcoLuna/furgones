0f87c3d1e5429bd4ece407c07279280b72ed6500
Anibal Flores <anibal.utem@gmail.com>
2015-11-13 13:52:44 -0300

    prueba

--- db/migrate/20151112014221_devise_create_usuarios.rb
+++ db/migrate/20151112014221_devise_create_usuarios.rb
@@ -4,7 +4,7 @@ class DeviseCreateUsuarios < ActiveRecord::Migration
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
-
+      t.integer :tipo
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151025154331) do
+ActiveRecord::Schema.define(version: 20151112014221) do
 
   create_table "alumnos", force: true do |t|
     t.string   "nombre"
@@ -53,5 +53,24 @@ ActiveRecord::Schema.define(version: 20151025154331) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "usuarios", force: true do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.integer  "tipo"
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+  end
+
+  add_index "usuarios", ["email"], name: "index_usuarios_on_email", unique: true, using: :btree
+  add_index "usuarios", ["reset_password_token"], name: "index_usuarios_on_reset_password_token", unique: true, using: :btree
 
 end
